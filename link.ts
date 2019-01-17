29ee1841820f9953e74e1db081035830b829d07c
Erokan Canbazoglu <Erokan@users.noreply.github.com>
2019-01-17 16:19:42 +0300

    Update link.ts
    changes from web

--- link.ts
+++ link.ts
@@ -1,3 +1,5 @@
 var link = "http://cdn.p30download.com/?b=p30dl-tutorial&f=Udemy.The.Complete.Python.and.JavaScript.Course.Build.Projects_p30download.com.part1.rar";
 var linkSayisi = 8;
 
+console.log(link);
+
